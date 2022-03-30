65fc371a357ae4ead491974b5f27080f361a07a5
stalink456 <stas.litvin2010@yandex.ru>
2022-03-30 12:42:02 +0300

    change params

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -16,7 +16,7 @@ function UserResults() {
         className="grid grid-cols-1 gap-8 xl:grid-cols-4
       lg:3grid-cols-3 md:grid-cols-2"
       >
-        {users.map((user, id) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
